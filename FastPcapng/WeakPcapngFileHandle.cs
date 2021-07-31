fb742bf39b1d7995198e0a8a537db9b52cc97454
theXappy <shaishapira@me.com>
2021-07-31 18:42:43 +0300

    WeakPcapngHandle - Use more of FastPcapngBinReader
    Replaced interface blocks parsing code with a call to the bin reader's already-implemented function that does the same

--- FastPcapng/WeakPcapngFileHandle.cs
+++ FastPcapng/WeakPcapngFileHandle.cs
@@ -29,7 +29,8 @@ namespace FastPcapng
 
         public List<long> GetPacketsOffsets()
         {
-            if (_cachedOffsets == null) {
+            if (_cachedOffsets == null)
+            {
                 using FileStream fs = new FileStream(Path, FileMode.Open, FileAccess.Read);
                 _cachedOffsets = _fastBinReader.ReadEpbOffsets(fs);
             }
@@ -38,37 +39,26 @@ namespace FastPcapng
 
         public List<InterfaceDescriptionBlock> GetInterfaces()
         {
-            if (_cachedIfacesBlock == null) {
+            if (_cachedIfacesBlock == null)
+            {
                 // TODO:
                 bool reverseByteOrder = false;
 
-                _cachedIfacesBlock = new List<InterfaceDescriptionBlock>();
-
-                // Checking until first packet block or end of file
                 using FileStream fileStream = File.OpenRead(Path);
                 using BinaryReader binReader = new BinaryReader(fileStream);
-                while (fileStream.Position != fileStream.Length) {
-                    // Read next 8 bytes of block:
-                    BaseBlock.Types type = (BaseBlock.Types)binReader.ReadUInt32();
-                    uint len = binReader.ReadUInt32();
-                    if (type == BaseBlock.Types.EnhancedPacket) {
-                        // Found a packet block, stopping search
-                        break;
-                    }
-
-                    if (type == BaseBlock.Types.InterfaceDescription) {
-                        fileStream.Seek(-8, SeekOrigin.Current);
-                        var block = AbstractBlockFactory.ReadNextBlock(binReader, reverseByteOrder, (ex) => Debug.WriteLine("Kek!" + ex));
-                        if (block == null || block.BlockType != BaseBlock.Types.InterfaceDescription) {
-                            throw new Exception("Block at given position was not parsed to a INTERFACE DESCRIPTION BLOCK");
-                        }
-                        _cachedIfacesBlock.Add(block as InterfaceDescriptionBlock);
-                    }
-                    else {
-                        // Advance to next block start
-                        fileStream.Seek(len - 8, SeekOrigin.Current);
-                    }
+                // Reading Section header to skip it
+                var (firstBlockType, firstBlockLen) = _fastBinReader.ReadBlockTypeLen(binReader);
+                if (firstBlockType != BaseBlock.Types.SectionHeader)
+                {
+                    // Error: First block must be section header
+                    return null;
                 }
+                byte[] sectionHeader = new byte[firstBlockLen];
+                binReader.BaseStream.Seek(0, SeekOrigin.Begin);
+                binReader.Read(sectionHeader, 0, (int)firstBlockLen);
+
+                // Interfaces
+                _cachedIfacesBlock = _fastBinReader.ReadInterfaceDescBlocks(binReader);
             }
             return _cachedIfacesBlock;
         }
@@ -79,23 +69,27 @@ namespace FastPcapng
             bool reverseByteOrder = false;
 
             using (FileStream fileStream = File.Open(Path, FileMode.Open, FileAccess.ReadWrite, FileShare.Read))
-            using (BinaryReader binReader = new BinaryReader(fileStream)) {
+            using (BinaryReader binReader = new BinaryReader(fileStream))
+            {
                 // Navigate to the overridden block start
                 long actualOffset = fileStream.Seek(packetBlockOffset, SeekOrigin.Begin);
                 // Make sure seek succeeded
-                if (actualOffset != packetBlockOffset) {
+                if (actualOffset != packetBlockOffset)
+                {
                     throw new Exception($"Couldn't seek to offset {packetBlockOffset}");
                 }
 
                 // Read next 8 bytes of the block we are overriding:
                 BaseBlock.Types type = (BaseBlock.Types)binReader.ReadUInt32();
-                if (type != BaseBlock.Types.EnhancedPacket) {
+                if (type != BaseBlock.Types.EnhancedPacket)
+                {
                     throw new Exception($"Expected an ENHANCED PACKET BLOCK (val:{BaseBlock.Types.EnhancedPacket}) in the given offset but got: {type}");
                 }
                 fileStream.Seek(-4, SeekOrigin.Current);
 
                 var block = AbstractBlockFactory.ReadNextBlock(binReader, reverseByteOrder, (ex) => Debug.WriteLine("Kek!" + ex));
-                if (block == null || block.BlockType != BaseBlock.Types.EnhancedPacket) {
+                if (block == null || block.BlockType != BaseBlock.Types.EnhancedPacket)
+                {
                     throw new Exception("Block at given position was not parsed to a ENHANCED PACKET BLOCK");
                 }
                 return block as EnhancedPacketBlock;
@@ -113,27 +107,33 @@ namespace FastPcapng
             byte[] data = newPacket.ConvertToByte(false, (ex) => Debug.WriteLine("LOL!" + ex));
 
             using (FileStream fileStream = File.Open(Path, FileMode.Open, FileAccess.ReadWrite))
-            using (BinaryReader binReader = new BinaryReader(fileStream)) {
+            using (BinaryReader binReader = new BinaryReader(fileStream))
+            {
                 // Navigate to the overridden block start
                 long actualOffset = fileStream.Seek(packetBlockOffset, SeekOrigin.Begin);
                 // Make sure seek succeeded
-                if (actualOffset != packetBlockOffset) {
+                if (actualOffset != packetBlockOffset)
+                {
                     throw new Exception($"Couldn't seek to offset {packetBlockOffset}");
                 }
 
                 // Read next 8 bytes of the block we are overriding:
                 BaseBlock.Types type = (BaseBlock.Types)binReader.ReadUInt32();
                 uint len = binReader.ReadUInt32();
-                if (type != BaseBlock.Types.EnhancedPacket) {
+                if (type != BaseBlock.Types.EnhancedPacket)
+                {
                     throw new Exception($"Expected an ENHANCED PACKET BLOCK (val:{BaseBlock.Types.EnhancedPacket}) in the given offset but got: {type}");
                 }
-                if (len < data.Length) {
+                if (len < data.Length)
+                {
                     ReplacePacketLonger(data, fileStream, len);
                 }
-                if (len > data.Length) {
+                if (len > data.Length)
+                {
                     ReplacePacketShorter(data, fileStream, len);
                 }
-                else {
+                else
+                {
                     // Lengths match exactly
                     fileStream.Seek(-4, SeekOrigin.Current);
                     fileStream.Write(data, 4, data.Length - 4);
@@ -145,7 +145,8 @@ namespace FastPcapng
         private static void ReplacePacketShorter(byte[] data, FileStream fileStream, uint len)
         {
             uint diff = len - (uint)data.Length;
-            if (diff % 4 != 0) {
+            if (diff % 4 != 0)
+            {
                 throw new NotImplementedException($"Trying to override with smaller packet but length diff is not multiply of 4");
             }
             // The trick: Adding unknown 'Option' blocks at the end of the packet
@@ -159,14 +160,17 @@ namespace FastPcapng
 
             // Number of DWORDs to add: the difference calculated + 1 for the removed end indicator in Options
             uint numOfDwordsToPad = diff / 4 + 1;
-            for (uint i = 0; i < numOfDwordsToPad; i++) {
+            for (uint i = 0; i < numOfDwordsToPad; i++)
+            {
                 // Option Type:
                 // * 0x00AA (unknown) if middle
                 // * 0x0000 (end indicator) if last
-                if (i == numOfDwordsToPad - 1) {
+                if (i == numOfDwordsToPad - 1)
+                {
                     fileStream.WriteByte(0x00);
                 }
-                else {
+                else
+                {
                     fileStream.WriteByte(0xAA);
                 }
                 fileStream.WriteByte(0x00);
@@ -187,7 +191,8 @@ namespace FastPcapng
             File.Copy(Path, tempFilePath);
 
 
-            using (FileStream tempFileStream = File.Open(tempFilePath, FileMode.Open, FileAccess.ReadWrite)) {
+            using (FileStream tempFileStream = File.Open(tempFilePath, FileMode.Open, FileAccess.ReadWrite))
+            {
                 // Move to the change position in the temp file
                 tempFileStream.Seek(fileStream.Position - 8, SeekOrigin.Begin);
                 // Write the new packet isntead of the old packet
@@ -196,7 +201,8 @@ namespace FastPcapng
                 // Move to start of NEXT block in original file
                 fileStream.Seek(len - 8, SeekOrigin.Current);
                 // Copy rest of file
-                while (fileStream.Position < fileStream.Length) {
+                while (fileStream.Position < fileStream.Length)
+                {
                     byte[] buff = new byte[Math.Min(1024, fileStream.Length - fileStream.Position)];
                     fileStream.Read(buff);
                     tempFileStream.Write(buff);
@@ -214,7 +220,8 @@ namespace FastPcapng
             // TODO: Reverse Byte order not always false probably...
             byte[] data = newPacket.ConvertToByte(false, (ex) => Debug.WriteLine("LOL!" + ex));
 
-            using (FileStream fileStream = File.Open(Path, FileMode.Append, FileAccess.ReadWrite)) {
+            using (FileStream fileStream = File.Open(Path, FileMode.Append, FileAccess.ReadWrite))
+            {
                 fileStream.Write(data);
             }
         }
